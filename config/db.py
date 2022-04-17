b804ac2f5b50c8f4babf6bcc99cd66b2574721d0
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2022-04-17 20:37:59 -0300

    Added Firebase Mock (WIP)

--- config/db.py
+++ config/db.py
@@ -17,8 +17,35 @@ else:
     _client = pymongo_inmemory.MongoClient()
     conn = _client.testdb
 
+
 # Firebase
-BUCKET = "spotifiuby-3c9fe.appspot.com"
-default_app = firebase_admin.initialize_app()
-storage_client = storage.Client()
-bucket = storage_client.bucket(BUCKET)
+class BlobMock:
+    c = ""
+
+    def upload_from_string(self, c):
+        self.c = c
+        return
+
+    def download_as_string(self):
+        return self.c
+
+
+class BucketMock:
+    d = dict()
+
+    def blob(self, s):
+        if s in self.d:
+            return self.d[s]
+        b = BlobMock()
+        self.d[s] = b
+        return b
+
+
+if os.getenv("CURRENT_ENVIRONMENT") == "production":
+    BUCKET = "spotifiuby-3c9fe.appspot.com"
+    default_app = firebase_admin.initialize_app()
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET)
+else:
+    default_app = firebase_admin.initialize_app()
+    bucket = BucketMock()
