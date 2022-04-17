a474efcec3a78cecbe688107427d618fc520def0
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2022-04-17 01:52:40 -0300

    Content upload and download

--- routes/content.py
+++ routes/content.py
@@ -1,13 +1,32 @@
-from fastapi import APIRouter, Response, HTTPException
-from service.content import get_song_content
+from fastapi import APIRouter, Response, HTTPException, UploadFile
+from service.content import get_song_content, upload_song_content
+import service.song
 
 content_routes = APIRouter()
 
 
+def _check_valid_song(song):
+    # return song and song["status"] == "active"
+    return True
+
+
 @content_routes.get("/songs/{song_id}/content", response_class=Response, tags=["Content"])
 async def get_content(song_id: str):
+    song = service.song.get(song_id)
+    if not _check_valid_song(song):
+        raise HTTPException(status_code=400, detail=f"Song not available {song_id}")
+
     contents = get_song_content(song_id)
     if contents:
         return Response(media_type="audio/mpeg3", content=contents)
     else:
         raise HTTPException(status_code=404, detail=f"Content not found for song {song_id}")
+
+
+@content_routes.post("/songs/{song_id}/content", response_class=Response, tags=["Content"])
+async def post_content(song_id: str, file: UploadFile):
+    song = service.song.get(song_id)
+    if not _check_valid_song(song):
+        raise HTTPException(status_code=400, detail=f"Song not available {song_id}")
+
+    upload_song_content(song_id, await file.read())
--- service/content.py
+++ service/content.py
@@ -11,3 +11,11 @@ def get_song_content(song_id: str):
     except exceptions.NotFound:
         return None
     return contents
+
+
+def upload_song_content(song_id: str, contents):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET)
+    blob = bucket.blob(f"{song_id}/{song_id}.mp3")
+
+    blob.upload_from_string(contents)
