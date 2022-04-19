7a5a868574f74d68e046d9ec4936afd0ab50cd30
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2022-04-18 21:28:13 -0300

    Added tests

--- tests/test_content.py
+++ tests/test_content.py
@@ -1,3 +1,5 @@
+import io
+
 import pytest
 from main import app
 from fastapi.testclient import TestClient
@@ -22,13 +24,30 @@ def test_get_content_not_found(mongo_test):
 
 
 def test_create_content(mongo_test):
-    with open("/Users/ndegiacomo/Downloads/file.mp3", "rb") as f:
-        response = client.post("/songs/625c9dcd232be00e5f827f7b/content", files={"file": ("file.mp3", f)})
+    f = io.StringIO("content")
+    response = client.post("/songs/625c9dcd232be00e5f827f7b/content", files={"file": ("file.mp3", f)})
     assert response.status_code == 201
 
 
 def test_get_content(mongo_test):
-    response = client.get("/songs/625c9dcd232be00e5f827f7c/content")
+    response = client.get("/songs/625c9dcd232be00e5f827f7b/content")
     assert response.status_code == 200
     assert response.content == b"content"
+    assert response.headers["content-type"] == "audio/mpeg3"
+
+
+def test_create_and_get_song_and_content(mongo_test):
+    response = client.post("/songs", json={"name": "test", "artist": "test"})
+    assert response.status_code == 201
+    assert len(response.json()) > 0
+    response.json()["name"] = "test"
+    song_id = response.json()["id"]
+
+    f = io.StringIO("test-content")
+    response = client.post(f"/songs/{song_id}/content", files={"file": ("file.mp3", f)})
+    assert response.status_code == 201
+
+    response = client.get(f"/songs/{song_id}/content")
+    assert response.status_code == 200
+    assert response.content == b"test-content"
     assert response.headers["content-type"] == "audio/mpeg3"
