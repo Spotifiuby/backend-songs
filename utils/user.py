6b33730f9dedf610b3d199885a6ee0058a5f006e
Fran Viñas <fvinas@fi.uba.ar>
2022-06-04 13:40:36 -0300

    add admin user permissions

--- utils/user.py
+++ utils/user.py
@@ -1,2 +1,13 @@
-def is_admin(user_id):
-    return False
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def is_admin(user_id, token_authorization):
+    x_api_key = os.getenv('BACKOFFICE_API_KEY')
+    r = requests.get(f'https://spotifiuby-api-gateway.herokuapp.com/users-api/users/{user_id}',
+                     headers={'x-api-key': x_api_key, 'Authorization': token_authorization})
+    user = r.json()
+    return user['user_type'] == 'admin'
